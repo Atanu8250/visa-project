a7c5ccde64abfc0aa8c7fd2013e779433dab5ba5
Atanu8250 <atanu20uparsole@gmail.com>
2023-07-31 17:11:43 +0530

    initial doc methods created

--- server/src/routes/docs.route.ts
+++ server/src/routes/docs.route.ts
@@ -3,6 +3,18 @@ import express, { Request, Response } from 'express';
 
 export const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-     res.send("Documents....");
-})
+router.route('/')
+     .get((req: Request, res: Response) => {
+          res.send("Documents....");
+     })
+     .post((req: Request, res: Response) => {
+          res.send("Documents....");
+     })
+
+router.route('/:docId')
+     .patch((req: Request, res: Response) => {
+          res.send("Documents....");
+     })
+     .delete((req: Request, res: Response) => {
+          res.send("Documents....");
+     })
